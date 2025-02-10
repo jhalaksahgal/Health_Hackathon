02dc44262205ba0fe5408ace38833f229032bc58
Aanandvyas <anandvyas0707@gmail.com>
2025-02-10 19:38:41 +0530

    a index change

--- server/index.js
+++ server/index.js
@@ -20,5 +20,5 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
